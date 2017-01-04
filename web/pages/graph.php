bed9b083059cdfe285470f8d2550c08f65468e60
Jonas Geschke <mail@yonny.de>
2017-01-04 17:52:14 +0100

    some refactoring

--- web/pages/graph.php
+++ web/pages/graph.php
@@ -1,15 +1,15 @@
-<script type="text/javascript" src="../charts.js"></script>
+<script type="text/javascript" src="charts.js"></script>
 <h3>Letzten 7 Tage</h3>
 <canvas id="lastWeekChart"> </canvas>
 <div style="border-bottom: 1px solid #ccc; height: 2px;"></div>
 <h3>Monatsübersicht</h3>
 <p style="margin-top: 15px;">
   <?php
-  $res = mysqli_query($con, "SELECT DISTINCT(YEAR(`time`)) as year FROM leistung WHERE DATE_FORMAT(`time`, '%m-%d') = '01-01' OR DATE_FORMAT(`time`, '%m-%d') = '12-31' ORDER BY `time` ASC;");
-  while($row = mysqli_fetch_array($res)) {
-    echo '<button class="btn btn-default" type="submit" onclick="getJSONFromUrl(\'api.php?q=year&y='.$row['year'].'\', updateYears);">'.$row['year'].'</button>';
-  }
-  mysqli_close($con);
+    $fgc = file_get_contents("http://".$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF'])."/api.php?q=yl");
+    $json = json_decode($fgc);
+    foreach ($json->{'series'} as $year) {
+      echo '<button class="btn btn-default" type="submit" onclick="getJSONFromUrl(\'api.php?q=year&y='.$year.'\', updateYears);">'.$year.'</button>';
+    } 
   ?>
   <button class="btn btn-default" type="submit" onclick="updateYearsChartAll()">Alle</button>
 </p>
